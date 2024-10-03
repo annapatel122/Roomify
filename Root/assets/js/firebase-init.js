3312d803b3227b571e65da94b4604297d7f3848a
Camisrutt <113075511+Camisrutt@users.noreply.github.com>
2024-10-03 00:45:41 +0000

    get auth change on init

--- Root/assets/js/firebase-init.js
+++ Root/assets/js/firebase-init.js
@@ -3,6 +3,7 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.14.0/firebase-app.js";
 import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.14.0/firebase-analytics.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.14.0/firebase-firestore.js";
+import { getAuth } from 'https://www.gstatic.com/firebasejs/10.14.0/firebase-auth.js';
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
