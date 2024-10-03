9a892dbf09fe7b13ef88e1b70ddea551b83999cc
Camisrutt <113075511+Camisrutt@users.noreply.github.com>
2024-10-03 15:54:37 +0000

    changed page relocation

--- Root/assets/js/signup.js
+++ Root/assets/js/signup.js
@@ -35,17 +35,17 @@ document.getElementById('signup-form').addEventListener('submit', async (e) => {
             uid: user.uid,
             username: username,
             email: email,
-            // Add other default profile fields here
             occupation: '',
             location: '',
             gender: '',
             moveInDate: '',
             budget: '',
             bio: ''
+        }).then(() => {
+            window.location.href = 'https://camisrutt.github.io/Roomifytest/Root/html-pages/profile-dash.html';
         });
+        // redirect to profile-dash.html
 
-        // Redirect to profile-dash.html
-        window.location.href = 'https://camisrutt.github.io/Roomifytest/Root/html-pages/profile-dash.html';
     } catch (error) {
         console.error('Error:', error.message);
         alert(error.message);
